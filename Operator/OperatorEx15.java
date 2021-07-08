810502c650a0ef69d7e6977ecec5aa02c29ba7dc
한규범 <53048655+HanQ0925@users.noreply.github.com>
2021-07-08 17:33:09 +0900

    2021-07-08 update
    JAVA의 정석 변수, 연산자 부분

---
+++ Operator/OperatorEx15.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorEx15 {
+
+	public static void main(String[] args) {
+		char lowerCase = 'a';
+		char upperCase = (char)(lowerCase -32);
+		System.out.println(upperCase);
+
+	}
+
+}
