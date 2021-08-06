e58423468058bbd0dd47b910766eaf3415ebec50
KB <be_man@naver.com>
2021-08-06 15:34:25 +0900

    printf 이해 및 정렬 이해

---
+++ java의 정석/src/Variable/variable.java
@@ -0,0 +1,17 @@
+package Variable;
+
+public class variable {
+	public static void main(String[] args) {
+		int year=0;
+		int age =14;
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+		year = year+2000;
+		age = age+1;
+		System.out.printf("finger:[%5d]%n", 10); //우측정렬
+		System.out.printf("finger:[%-5d]%n", 10); //좌측정렬
+		System.out.printf("finger:[%05d]%n", 10);
+	}
+}
