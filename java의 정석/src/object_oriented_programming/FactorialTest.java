6dc131c4181c0d6ab87db23ae3e452128e45f393
KB <be_man@naver.com>
2021-08-09 16:20:59 +0900

    factorial 재귀호출 구현

---
+++ java의 정석/src/object_oriented_programming/FactorialTest.java
@@ -0,0 +1,20 @@
+package object_oriented_programming;
+
+public class FactorialTest {
+
+	public static void main(String[] args) {
+		int result = factorial(4);
+		
+		System.out.println(result);
+		
+	}
+	
+	static int factorial(int n) {
+		int result = 0;
+		if(n==1)
+			result=1;
+		else
+			result=n*factorial(n-1);
+		return result;
+	}
+}
