5f74360c2bba9ee09c78844eea707044f826b306
KB <be_man@naver.com>
2021-08-09 16:15:51 +0900

    반환값이 메서드가 되는 경우

--- java의 정석/src/object_oriented_programming/ReferenceReturnEx.java
+++ java의 정석/src/object_oriented_programming/ReferenceReturnEx.java
@@ -1,22 +1,22 @@
 package object_oriented_programming;
 
-class Data{int x;}
+class Data1{int x;}
 
 public class ReferenceReturnEx {
 
 	public static void main(String[] args) {
-		Data d = new Data();
+		Data1 d = new Data1();
 		
 		d.x = 10;
 		
-		Data d2 = copy(d);
+		Data1 d2 = copy(d);
 		
 		System.out.println("d.x = "+d.x);
 		System.out.println("d2.x = "+d2.x);	
 	}
 	
-	static Data copy(Data d) {
-		Data tmp = new Data();
+	static Data1 copy(Data1 d) {
+		Data1 tmp = new Data1();
 		tmp.x = d.x;
 		
 		return tmp;
