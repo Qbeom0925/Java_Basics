5e56e5d912552d136249e91d61e50322f4f58cb4
KB <be_man@naver.com>
2021-08-09 10:39:38 +0900

    매개변수가 참조형인 경우 값 변경이 가능

---
+++ java의 정석/src/object_oriented_programming/ReferenceParamEx.java
@@ -0,0 +1,22 @@
+package object_oriented_programming;
+
+class Data {int x;}
+
+public class ReferenceParamEx {
+
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("main() : x = "+d.x);
+		
+		change(d);
+		System.out.println("After change(d.x)");
+		System.out.println("main() : x = "+d.x);		
+	}
+
+	static void change(Data d) {
+		d.x = 1000;
+		System.out.println("change() : x = "+d.x);
+	}
+
+}
