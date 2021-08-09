0e09125b6f6213ae1f3253dfb3d882f5faad6977
KB <be_man@naver.com>
2021-08-09 10:34:04 +0900

    메서드 기본형 매개변수가 변수의 값을 변경 불가

---
+++ java의 정석/src/object_oriented_programming/PrimitiveParaEx.java
@@ -0,0 +1,23 @@
+package object_oriented_programming;
+
+class Data {int x;}
+
+public class PrimitiveParaEx {
+
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("main() : x = "+d.x);
+		
+		change(d.x);
+		System.out.println("After change(d.x)");
+		System.out.println("main() : x = "+d.x);
+	}
+
+	static void change(int x) { //기본형 매개변수
+		x=1000;
+		System.out.println("change() : x = "+x);
+		
+	}
+
+}
