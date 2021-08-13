3b393cb411a55f720ecb4ec244ef8ff40c9136dd
KB <be_man@naver.com>
2021-08-13 09:47:47 +0900

    제품에 생산일련번호를 부여하는 것과 같이 클래스 의 인스턴스를 생성할 때 마다 인스턴스 블럭의 수행

---
+++ java의 정석/src/object_oriented_programming/ProductTest.java
@@ -0,0 +1,27 @@
+package object_oriented_programming;
+
+class Product{
+	static int cnt = 0;
+	int serialNo;
+	{
+		++cnt;
+		serialNo = cnt;
+	}
+	
+	public Product() {}
+}
+
+public class ProductTest {
+	public static void main(String[] args) {
+	Product p1 = new Product();
+	Product p2 = new Product();
+	Product p3 = new Product();
+	
+	System.out.println("p1의 제품번호 (serial no)는 "+p1.serialNo);
+	System.out.println("p2의 제품번호 (serial no)는 "+p2.serialNo);
+	System.out.println("p3의 제품번호 (serial no)는 "+p3.serialNo);
+		
+		
+	}
+
+}
