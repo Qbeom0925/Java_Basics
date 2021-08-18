14d6c2482da0c63e3166c68d8fae92b69127f603
KB <be_man@naver.com>
2021-08-18 09:37:11 +0900

    super 참조변수의 이해

---
+++ java의 정석/src/object_oriented_programming2/SuperTest.java
@@ -0,0 +1,22 @@
+package object_oriented_programming2;
+
+public class SuperTest {
+
+	public static void main(String[] args) {
+		Child c = new Child();
+		c.method();
+		
+	}
+}
+
+class Parent{
+	int x = 10;
+}
+
+class Child extends Parent{
+	void method() {
+		System.out.println("x="+x);
+		System.out.println("this.x="+this.x);
+		System.out.println("super.x="+super.x);
+	}
+}
