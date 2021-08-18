efbefe6db3a972516a84e5f5a4c969da66bf95dd
KB <be_man@naver.com>
2021-08-18 09:40:32 +0900

    this와 super의 참조변수 차이 이해

---
+++ java의 정석/src/object_oriented_programming2/SuperTest2.java
@@ -0,0 +1,22 @@
+package object_oriented_programming2;
+
+public class SuperTest2 {
+	public static void main(String[] args) {
+		Child2 c = new Child2();
+		c.method();
+	}
+}
+
+class Parent2{
+	int x=10;
+}
+
+class Child2 extends Parent{
+	int x=20;
+	
+	void method() {
+		System.out.println("x="+x);
+		System.out.println("this.x"+x);
+		System.out.println("super.x"+x);		
+	}
+}
