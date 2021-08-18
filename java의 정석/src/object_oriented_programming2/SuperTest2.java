a1b5a9d6aa4bde3df953827fb5df3277a45a0a02
KB <be_man@naver.com>
2021-08-18 09:46:12 +0900

    this super 참조변수 이해

--- java의 정석/src/object_oriented_programming2/SuperTest2.java
+++ java의 정석/src/object_oriented_programming2/SuperTest2.java
@@ -16,7 +16,7 @@ class Child2 extends Parent{
 	
 	void method() {
 		System.out.println("x="+x);
-		System.out.println("this.x"+x);
-		System.out.println("super.x"+x);		
+		System.out.println("this.x="+this.x);
+		System.out.println("super.x="+super.x);		
 	}
 }
