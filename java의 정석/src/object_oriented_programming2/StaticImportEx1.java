2c6a2157fad2d7e4095fe75c4ff4f13c70969735
KB <be_man@naver.com>
2021-08-19 17:27:04 +0900

    static import 활용

---
+++ java의 정석/src/object_oriented_programming2/StaticImportEx1.java
@@ -0,0 +1,14 @@
+package object_oriented_programming2;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class StaticImportEx1 {
+
+	public static void main(String[] args) {
+		out.println(random());
+		
+		out.println("Math.PI :"+PI);
+	}
+
+}
