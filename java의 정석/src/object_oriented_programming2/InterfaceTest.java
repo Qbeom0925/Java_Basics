e92a1d8ab47791f1af7cd3f5e5c7072bcc359d64
KB <be_man@naver.com>
2021-08-30 11:43:19 +0900

    인터페이스의 이해

---
+++ java의 정석/src/object_oriented_programming2/InterfaceTest.java
@@ -0,0 +1,22 @@
+package object_oriented_programming2;
+
+class A{
+	public void methodA(B b) {
+		b.methodB();
+	}
+}
+
+class B{
+	public void methodB() {
+		System.out.println("methodB()");
+	}
+}
+
+public class InterfaceTest {
+
+	public static void main(String[] args) {
+		A a = new A();
+		a.methodA(new B());
+	}
+
+}
